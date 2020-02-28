a160783480267ef719d529eea358678ff29265ad
vincius <vincius.cavalcanti16@gmail.com>
2020-02-28 10:23:09 -0300

    testes de pre-push

--- application/src/main/java/gs/hexagonaldemo/springhexagonaldemo/configuration/ApplicationConfiguration.java
+++ application/src/main/java/gs/hexagonaldemo/springhexagonaldemo/configuration/ApplicationConfiguration.java
@@ -10,7 +10,7 @@ import gs.hexagonaldemo.springhexagonaldemo.serviceadapters.GetUserServiceAdapte
 import gs.hexagonaldemo.springhexagonaldemo.verifiers.NameVerifierAdapter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+//testes
 @Configuration
 public class ApplicationConfiguration {
 
